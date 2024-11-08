b37e074b66166956524c05a6586b485fdaaecdc2
Robetgarcia <caiofbgame@gmail.com>
2024-11-08 10:22:05 -0300

    Update atividade1.c

--- atividade1.c
+++ atividade1.c
@@ -2,22 +2,29 @@
 
 int main()
 {
-    int valor;
+ int valor [3];
+ int i;
+    for(i=0; i<3; i++)
+    {
     printf("Coloque um valor:\n");
-    scanf("%d"&valor);
-
+    scanf("%d",&valor [i]);
+    }
+    
+    for(i=0; i<3; i++)
+    {
     if (valor > 0)
     {
-        printf("o numero é maior que zero.")
+        printf("%d numero é maior que zero.", valor [i]);
     }
     
     else if ((valor < 0))
     {
-        printf("o numero é maior que zero.")
+        printf("%d numero é menor que zero.", valor [i]);
     }
 
     else if ((valor == 0))
     {
-        printf("o numero é maior que zero.")
+        printf("%d numero é igual que zero.", valor [i]);
+    }
     }
 }
