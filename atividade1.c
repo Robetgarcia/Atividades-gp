5b3d35d2953314fd2616bb156b66d67670ac44c9
Aluno <aluno@alunos.ce.senac.br>
2024-11-08 09:16:01 -0300

    teste programa

---
+++ atividade1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int valor;
+    printf("Coloque um valor:\n");
+    scanf("%d"&valor);
+
+    if (valor > 0)
+    {
+        printf("o numero é maior que zero.")
+    }
+    
+    else if ((valor < 0))
+    {
+        printf("o numero é maior que zero.")
+    }
+
+    else if ((valor == 0))
+    {
+        printf("o numero é maior que zero.")
+    }
+}
